8b4da4135654110fee6e2ea43124776d0ea8533a
Souleyman <smahamat1@mail.pima.edu>
2024-09-22 22:48:06 -0700

    Add bagel, tea, and thank you message.

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -3,6 +3,8 @@
 # set constants: prices and tax rate
 coffee_price = 5
 muffin_price = 4
+bagel_price = 2
+tea_price = 2
 tax_rate = 0.06
 
 print('***************************************\nMy Coffee and Muffin Shop')
@@ -10,13 +12,17 @@ print('***************************************\nMy Coffee and Muffin Shop')
 # ask for numbers of coffees and muffins bought
 coffee_count = int(input('Number of coffees bought?\n'))
 muffin_count = int(input('Number of muffins bought?\n'))
+bagel_count = int(input('Number of bagels bought?\n'))
+tea_count = int(input('Number of teas bought?\n'))
 
 print('***************************************\n')
 
 # crunch numbers
 coffee_total = coffee_price*coffee_count
 muffin_total = muffin_price*muffin_count
-subtotal = coffee_total + muffin_total
+bagel_total = bagel_price*bagel_count
+tea_total = tea_price*tea_count
+subtotal = coffee_total + muffin_total + bagel_total + tea_total
 tax_total = subtotal * tax_rate
 tax_total_string = str(tax_total)  # string form to be able to later print without leading 0 if necessary
 total = subtotal + tax_total
@@ -35,6 +41,18 @@ if muffin_count == 1:
 else:
     print(f"{muffin_count} Muffins at ${muffin_price} each: $ {muffin_total:.2f}")
 
+# change message between plural and singular
+if bagel_count == 1:
+    print(f"{bagel_count} Bagel at ${bagel_price} each: $ {bagel_total:.2f}")
+else:
+    print(f"{bagel_count} Bagels at ${bagel_price} each: $ {bagel_total:.2f}")
+
+# change message between plural and singular
+if tea_count == 1:
+    print(f"{tea_count} Tea at ${tea_price} each: $ {tea_total:.2f}")
+else:
+    print(f"{tea_count} Teas at ${tea_price} each: $ {tea_total:.2f}")
+
 # print tax without leading zero if necessary
 if 1 > tax_total > 0:
     print(f"{tax_rate*100}% tax: $ .{tax_total_string[2]}{tax_total_string[3]}")
@@ -42,3 +60,5 @@ else:
     print(f"{tax_rate*100}% tax: $ {tax_total:.2f}")
 
 print(f"---------\nTotal: $ {total:.2f}\n***************************************")
+
+print('Thank you')
